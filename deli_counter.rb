7a32cff9ecc271154fa8dbb45b8da0336e9e98d5
Mac Rowe <m.rowe42@gmail.com>
2021-02-24 20:21:38 -0500

    commit tree 5d4f7c4fb72ac626f2a37ab278efb433d67d93c2

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+def line(array)
+    if array.length == 0
+        puts "The line is currently empty."
+    else 
+        phrase = "The line is currently:"
+        array.each.with_index(1) do | person, number_in_line |
+            phrase << " #{number_in_line}. #{person}"
+        end
+        puts phrase
+    end
+end
+
+def take_a_number(array, name)
+    array.push(name)
+    number_in_line = array.count
+    puts "Welcome, #{name}. You are number #{number_in_line} in line."
+    return array
+end
+
+def now_serving(array)
+    if array.length == 0
+        puts "There is nobody waiting to be served!"
+    else 
+        customer_name = array.shift
+        puts "Currently serving #{customer_name}."
+    return array
+    end
+end
